d6897937be1580c8bb8ccde5a58e02bebbefd33f
sefasaygan <34549737+sefasaygan@users.noreply.github.com>
2019-11-23 17:51:21 +0300

    Add files via upload

---
+++ uslusayi.php
@@ -0,0 +1,13 @@
+<?php
+// üslü sayı formülü
+// yapım:sefa sayğan
+
+$sayi=4; // Gerçek sayıyı belirtir 
+$ussu=4; //Üslü sayıyı belirtir 
+$islem=1; // işlemleri çarparak değişkende tutmayı sağlar 
+for($i=0;$i<$ussu;$i++){ 
+	$islem=$islem*$sayi; 
+} 
+	echo $islem;
+ 
+?>
